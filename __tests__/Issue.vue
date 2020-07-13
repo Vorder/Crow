a33e60a029831dc6b9233df83192c808b9fe45e9
Ati-ns <camelia.s77@gmail.com>
2020-07-13 23:02:59 +0430

    Write some test for issue

---
+++ __tests__/Issue.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id='rec' :style="{ width: width+'px' } " @wheel="zoom($event)"></div>
+</template>
+
+<script>
+import json from './data.json'
+
+export default {
+  props:{
+      widths: Number
+  },
+  data (){
+    return {
+            width: 40,
+            scale: 1,
+            myJson: json
+        }
+  },
+  methods: {
+      zoom(event) {
+           this.scale += event.deltaY * -0.01;
+
+          // Restrict scale
+          this.scale = Math.min(Math.max(.05, this.scale), 4);
+          const el = document.getElementById('rec');
+          // Apply scale transform
+          el.style.transform = `scaleX(${this.scale})`;
+        },
+  },
+  mounted(){
+    this.width = this.myJson.days * 40
+  }
+}
+</script>
+
+<style>
+#rec {
+  width: 40px;
+  height: 20px;
+  border: 2px solid blue;
+}
+</style>
---
+++ __tests__/issue.test.js
@@ -0,0 +1,47 @@
+
+import { shallowMount } from '@vue/test-utils'
+import Issue from './Issue.vue'
+
+const wrapper = shallowMount(Issue)
+
+describe('Issue', () => {
+  const div = wrapper.find('div')
+    
+  it('find div', ()=>{
+    expect(div.exists()).toBe(true)
+    expect(div.element.id).toBe('rec')
+    
+  }) 
+
+  it('check width',()=>{
+    wrapper.setData({ width: 40 })
+
+    expect(wrapper.vm.width).toBe(40)
+  })
+  
+  it('check scale',()=>{
+    expect(wrapper.vm.scale).toBe(1)
+  })
+
+  it('check json', ()=>{
+
+    expect(wrapper.vm.myJson).toEqual({
+        "issue1": {   
+          "days": 5,
+          "startDate": "2020-07-07",   
+          "endDate": "2020-07-12"
+        }
+    })
+
+  })
+
+  it('check zoom by wheel', async () => {
+    const div= wrapper.find('div')
+        
+    await div.trigger('wheel')
+    expect(wrapper.vm.width).toBe(40)
+    expect(wrapper.find('div').attributes().style).toBe("width: 40px;")
+    console.log(div)
+  })
+
+})
