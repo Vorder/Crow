28918c0ab02194ad969ba83b90165ff9ecff16b1
Ati-ns <camelia.s77@gmail.com>
2020-07-22 21:54:14 +0430

    Add backlog component

---
+++ src/components/Backlog.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="logcontainer">
+    <h2>BackLog</h2>
+    <div id="log">
+        <div class="issue_item" v-for="(item, index) in issue" :key="index">
+            {{ item }}
+        </div>
+    </div>
+</div>
+</template>
+<script>
+import issues from '../../server_data/untrack_issues.json'
+
+export default {
+    data () {
+        return {
+            issue: issues
+        }
+    }    
+}
+</script>
+<style>
+.logcontainer {
+    text-align: left;
+}
+#log {
+    border: 2px solid gray;
+    height: 800px;
+    width: 100%;
+}
+.issue_item {
+    margin: 10px;
+}
+.h2 {
+    font-size: 1.5em;
+}
+</style>
