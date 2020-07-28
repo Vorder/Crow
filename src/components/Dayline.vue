c99d89b4fb7804aaeaceaebe6001694a84201a77
Ati-ns <camelia.s77@gmail.com>
2020-07-28 18:07:07 +0430

    Separate days timeline from main component

---
+++ src/components/Dayline.vue
@@ -0,0 +1,190 @@
+<template>
+    <div id="container">
+        <div class="firstblock">
+            Date Developers
+        </div>
+        <div class="dev" v-for="(dev, index) in developers" :key="dev"
+        :style="{ 'grid-row': index+4 }">
+            {{ dev }}
+        </div>
+        <div class="date" v-for="(item, index) in calendar()" 
+        :key="item+index" 
+        :style="{ 'grid-column': index+2 }">
+            <p>{{ item.month }}</p>
+            <p>{{ item.day }}</p>
+        </div>
+        <div v-for="item in issuelist" :key="item.id" 
+        :style="{ 'grid-area': area(item) }">
+            <Issue :name=item.name></Issue>
+        </div>
+        <div class="lines" v-for="lin in lines" :key="lin[0]+lin[2]">
+            <Dline 
+            :startrow="lin[0]" 
+            :endrow="lin[1]"
+            :startdate="lin[2]"
+            :enddate="lin[3]"
+            :datewidth=80
+            :gaplength=20></Dline>
+        </div>
+    </div>
+</template>
+
+<script>
+import issues from '../../server_data/issues.json'
+import Issue from './Issue.vue'
+import Dline from './Line.vue'
+
+const month = new Array("Jan", "Feb", "Mar", "Apr", "May", 
+"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+export default {
+    components: {
+        Issue,
+        Dline
+    },
+    data () {
+        return{
+            scale : 1,
+            issuelist: issues,
+        }
+    },
+    props: {
+        developers: Array,
+        startdate: Date,
+        enddate: Date
+    },
+    methods:{
+        find_date (issue) {
+            var d = new Date(issue.startTime);
+            var left = Math.round((d - (new Date(this.startdate)))/this.day_length)+1;
+            return left;
+        },
+        area (issue) {
+            var column_start = this.find_date(issue)+1 
+            var column_end = issue.days;
+            var row = issue.developerNum+3;
+            var a = '' + row + '/' + column_start + '/' + row +  '/span ' + column_end;
+            return a;  
+        },
+        goToday(){
+            var l = document.getElementById("container");
+            l.scrollLeft += ((this.today - this.startdate)/this.day_length)*80 - 560; 
+        },
+        calendar (){
+            var dayss = new Array(this.calendar_length);
+            var counter = new Date(this.startdate);
+            for (let i = 0; i <= this.calendar_length ; i++){
+                if(counter.getDate() == this.today.getDate() &&
+                   counter.getMonth()==this.today.getMonth() &&
+                   counter.getFullYear() == this.today.getFullYear()) {
+                    dayss[i] = { 
+                        month: month[this.today.getMonth()],
+                        day: this.today.getDate()+ "\nToday"
+                        };
+                }
+                else {
+                    dayss[i] = { 
+                        month: month[counter.getMonth()],
+                        day: counter.getDate()
+                    }
+                }
+                counter.setDate(counter.getDate() + 1);
+            }
+            return dayss
+        },
+    },
+    computed: {
+        day_length: function() {
+            var one_day=1000*60*60*24;
+            return one_day;
+        },
+        today: function() {
+            return new Date();
+        },
+        calendar_length: function() {
+            return (this.enddate - this.startdate)/this.day_length
+        },
+        lines: function(){
+            var n = new Array();
+            for(let i=0; i<issues.length; i++){
+                if(issues[i].relatedTo.length !=0){
+                    for(let j=0; j<issues[i].relatedTo.length; j++){
+                        for(let k=0; k<issues.length; k++){
+                            if(issues[k].id == issues[i].relatedTo[j]){
+                                n.push([issues[k].developerNum, issues[i].developerNum,
+                                 issues[k].endTime, issues[i].startTime])
+                            }
+                        }
+                    }
+                }
+            }
+            return n;
+        }
+    },
+    mounted(){
+        // this.goToday();
+    }
+}
+</script>
+
+<style>
+#container {
+    display: grid;
+    position: relative;
+    border: 2px solid gray;
+    width: 1150px;
+    height: 700px;
+    /* grid-gap:x 5px 50px; */
+    /* grid-template: repeat(5, 30px) / repeat(120, 80px); */
+    overflow: auto;
+    text-align: center;
+}
+.item {
+    border: 2px solid blue;
+    grid-column: 1 / 4;
+    grid-column: 1 / span 3;
+    grid-row: 2 / 5;
+    grid-area: 1 / 1 / 4 / 5;
+}
+.firstblock {
+    grid-row: 1 / 4;
+    grid-column: 1;
+    position: sticky;
+    top:0;
+    z-index: 10;
+    background-color: rgb(135, 222, 181);
+    padding: 5px;
+    font-size: 1em;
+    border-right: 2px solid black;
+    border-bottom: 2px solid black;
+}
+.date {
+    grid-row: 1 / 4;
+    width: 80px;
+    padding: 0px 20px;
+    height: 100px;
+    display: inline-block;
+    border-right: 2px solid gray;
+    border-bottom: 2px solid black;
+    background-color: blanchedalmond;
+    text-align: center;
+    position: sticky;
+    top:0;
+    z-index: 10;
+}
+.dev {
+    width: 120px;
+    height: 50px;
+    padding: 5px;
+    border-right: 2px solid black;
+    border-bottom: 2px solid grey;
+    text-align: center;
+    position: sticky;
+    left: 0;
+    background-color: white;
+    z-index: 3;
+}
+.lines {
+    position: absolute;
+}
+</style>
---
+++ src/components/Timelines.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="row">
+    <div class="col-sm-12">
+        <h2>TimeLine</h2>
+        <keep-alive>
+            <div @wheel.ctrl="zoom($event)">
+                <component v-bind:is="timeline" 
+                :startdate= startdate 
+                :enddate = enddate
+                :developers= developers>
+                </component>
+            </div>
+        </keep-alive>
+        <div class="row, endtime">
+            <h3>Estimated end time: {{ endAll }}</h3>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import devs from '../../server_data/developers.json'
+import dates from '../../server_data/calendar.json'
+import Dayline from './Dayline.vue'
+import Weekline from './Weekline.vue'
+
+
+export default {
+    components: {
+        Dayline,
+        Weekline
+    },
+    data () {
+        return{
+            scale : 1,
+            developers: devs,
+            startdate: null,
+            enddate: null,
+            endAll : "2020-09-9",
+            // timelines: ["Hourline", "Dayline", "Weekline", "Monthline", "Yearline"]
+            timelines: ["Weekline", "Dayline", "Weekline", "Dayline", "Weekline"]
+        }
+    },
+   
+    computed: {
+        timeline: function(){
+            return this.timelines[this.scale]
+        },
+        day_length: function() {
+            var one_day=1000*60*60*24;
+            return one_day;
+        },
+        today: function() {
+            return new Date();
+        },
+        calendar_length: function() {
+            return (this.enddate - this.startdate)/this.day_length
+        },
+    },
+    mounted(){
+        this.startdate = new Date(dates.start);
+        this.enddate = new Date(dates.end);
+    },
+    methods:{
+        zoom(event){
+            event.preventDefault();
+
+            this.scale += event.deltaY > 0  ? (this.scale < 4 ? 1: 0) : (this.scale>0 ? -1 : 0); 
+        },      
+    }
+}
+</script>
+
+<style>
+.endtime {
+    text-align: center;
+    margin-top: 30px;
+}
+</style>
