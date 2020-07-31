1de0c2b9d21393ca95f2ac3e7cadaebe626ff7c9
Ati-ns <camelia.s77@gmail.com>
2020-07-31 21:56:31 +0430

    Some change in color

--- src/components/Issue.vue
+++ src/components/Issue.vue
@@ -2,7 +2,8 @@
     <div class="issue" :style="{ 'border-left': border + 'px solid white', 
                                  'border-right': border + 'px solid white',
                                  'font-size': textsize + 'em' }">
-      {{ name }} </div>
+      {{ name }}
+    </div>
 </template>
 
 <script>
@@ -22,11 +23,11 @@ export default {
   border: 4px solid white;
   border-right: 20px solid white ;
   border-left: 20px solid white ;
-  background-color: #283593;
-  color: #ffffe4;
+  background-color: #42a5f5;
+  color: black;
   padding-top: 5px;
   z-index: 2;
-  text-shadow: 0px 0px 3px black;
+  /* text-shadow: 0px 0px 3px black; */
   font-size: 1em;
 }
 </style>
