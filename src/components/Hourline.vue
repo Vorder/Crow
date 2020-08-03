fbde0ab065005f716504d28b8bb0ecd152330588
Ati-ns <camelia.s77@gmail.com>
2020-08-04 03:32:06 +0430

    Fix height of date in timeline

--- src/components/Hourline.vue
+++ src/components/Hourline.vue
@@ -160,7 +160,7 @@ export default {
     grid-row: 1 / 2;
     width: 720px;
     padding: 10px 20px;
-    height: 50px;
+    height: 40px;
     display: inline-block;
     border-right: 2px solid #b0bec5;
     background-color: #ffe0b2;
@@ -181,7 +181,7 @@ export default {
     background-color: #ffe0b2;
     text-align: center;
     position: sticky;
-    top:50px;
+    top:0;
     z-index: 12;
 }
 .hour {
