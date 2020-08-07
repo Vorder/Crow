182a3eab37c1af718f6b4fa4ddb45225e55dfeda
Ati-ns <camelia.s77@gmail.com>
2020-08-07 20:19:25 +0430

    Fix overflow in issue

--- src/components/Issue.vue
+++ src/components/Issue.vue
@@ -2,7 +2,7 @@
     <div class="issue" :style="{ 'border-left': border + 'px solid white', 
                                  'border-right': border + 'px solid white',
                                  'font-size': textsize + 'em' }">
-      <span class="isstext"> {{ name }} </span>
+      <div class="isstext"><span class="inner"> {{ name }} </span></div>
     </div>
 </template>
 
@@ -26,13 +26,23 @@ export default {
   background-color: #42a5f5;
   color: black;
   padding: 5px;
-  z-index: 2;
   /* text-shadow: 0px 0px 3px black; */
   font-size: 1em;
   text-align: left;
 }
-.isstext{
+.issue:hover ~ .isstext.inner{
+  overflow: visible;
   position: sticky;
   left: 122px;
+}
+.isstext{
+  overflow: hidden;
+  text-overflow: ellipsis;
+  z-index: 2;
+  position: sticky;
+  left: 122px;
+}
+.isstext:hover{
+  overflow: visible;;
 }
 </style>
